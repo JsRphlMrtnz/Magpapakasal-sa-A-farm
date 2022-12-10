ed795bbd65f4642b33f48a6dfe8b3d7382aabda5
Rafa <58285574+JsRphlMrtnz@users.noreply.github.com>
2022-12-10 12:21:55 +0800

    Delete tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++
@@ -1,3 +0,0 @@
-      // this.farmerPic.setBorder(BorderFactory.createLineBorder(Color.black));
-      // this.farmerPic.setMinimumSize(new Dimension(50, 100));
-      // this.farmerPic.setMaximumSize(new Dimension(50, 100));
